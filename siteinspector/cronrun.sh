0def334c776f612f5d1b5dd604b8374a5ac584b7
ReinVelt <rein@velt.org>
2014-02-06 16:20:01 +0100

    update cron

--- siteinspector/cronrun.sh
+++ siteinspector/cronrun.sh
@@ -8,12 +8,12 @@ export ITEMS=`/usr/bin/php -f nutch2quail.php`
 for i in $ITEMS
 do
 
-  #CHECK SITE
+  #CHECK SITE AND WRITE TESTRESULTS TO SOLR (PHANTOMCORE)
   /opt/siteinspector/checksite.sh $i
 
-  #UPDATE TEST FIELD IN SOLR NUTCH RECORD
-  export UPDATE='[{"url":"$1","id":"$1","tested":{"set":1}}]'
+  #UPDATE TEST FIELD IN SOLR NUTCH RECORD (COLLECTION1)
+  export UPDATE='[{"url":"$i","id":"$i","tested":{"set":1}}]'
   curl 'localhost:8983/solr/update?commit=true' -H 'Content-type:application/json' -d $UPDATE
-
+  echo $UPDATE
 done
 #BATCH READY
