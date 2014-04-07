85d52c73f860b64e8e25040a75757fbec439273a
Jur de Vries <jur@triquanta.nl>
2014-04-07 13:25:06 +0200

    minor bugfix

--- siteinspector/checksite.sh
+++ siteinspector/checksite.sh
@@ -2,5 +2,5 @@
 mkdir -p /tmp/quaildata
 export FILENAME=`/usr/bin/php -f /opt/siteinspector/createhash.php $1`
 echo $1 > /tmp/quaildata/$FILENAME
-/usr/local/bin/phantomjs/phantomjs --ignore-ssl-errors=yes /opt/siteinspector/phantomquail.js $1 >> /tmp/quaildata/$FILENAME
+/usr/local/bin/phantomjs --ignore-ssl-errors=yes /opt/siteinspector/phantomquail.js $1 >> /tmp/quaildata/$FILENAME
 /usr/bin/php -f /opt/siteinspector/parseData.php /tmp/quaildata/$FILENAME
